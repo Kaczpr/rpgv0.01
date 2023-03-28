91b62f0fc602f9e88952e942bc0864853694ba93
Kacper <bojarski.kacper02@gmail.com>
2023-03-28 21:08:26 +0200

    ***

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    //hero protag = new hero();
-    //protag.create();
-    enemy maciek = new enemy();
-        System.out.println(maciek.S);
-        game.pressEnterKeyToContinue();
-        System.out.println(maciek.mS);
+    hero protag = new hero();
+    protag.create();
     }
 }
